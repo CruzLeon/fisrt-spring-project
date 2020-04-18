3b84f4bf0770672707b434f75bd8100eee5b0aa4
Leonardo <cruz.leong95@gmail.com>
2020-04-18 18:33:56 -0500

    release project

---
+++ src/main/java/com/learnSpring/first/projectspring/Application.java
@@ -0,0 +1,13 @@
+package com.learnSpring.first.projectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
